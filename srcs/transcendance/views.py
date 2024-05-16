a31220277858b3a8b95973488f077827c0c4de0f
Nily <nghulam@student.42.fr>
2024-05-16 13:23:34 +0200

    changed about template structure ; added classes to organize about template data ; harmonized images and classes names

--- srcs/transcendance/views.py
+++ srcs/transcendance/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
+from .models import Author
 
 # Create your views here.
 
 def index(request) :
-    return (render(request, "index.html"))
+    return render(request, "index.html")
 
 def rules(request) :
-    return (render(request, "rules.html"))
+    return render(request, "rules.html")
 
 def about(request) :
-    return (render(request, "about.html"))
+    data = dict({"authors": [Author("Nily"), Author("Nassima"), Author("Theo")]})
+    return render(request, "about.html", data)
 
 def play(request) :
-    return (render(request, "play.html"))
+    return render(request, "play.html")
