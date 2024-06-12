329fa9a2f6edfbaec660041f02405b682ab6f6c2
Theo BAE <tbae@paul-f4Br5s4.clusters.42paris.fr>
2024-06-12 15:26:39 +0200

    move padle and collison

--- srcs/pong/templates/pong/main.js
+++ srcs/pong/templates/pong/main.js
@@ -42,20 +42,47 @@ const controls = new OrbitControls(camera, renderer.domElement)
 
 let direction = 1;
 
-function animate() {
-	requestAnimationFrame( animate );
-
-	//ball.object.rotation.x -= 0.1
-	ball.object.rotation.y += 0.1 * direction
-	ball.object.position.x += 0.1 * direction;
-	ball.pointLight.position.x += 0.1 *direction;
-	if (ball.object.position.x >= 25 || ball.object.position.x <= -25)
+function colision()
+{
+	if (ball.object.position.x + 2.5 >= paddleRight.object.position.x - 5 && ball.object.position.y <= paddleRight.object.position.y + 15 && ball.object.position.y >= paddleRight.object.position.y - 15)
+		direction *= -1
+	if (ball.object.position.x - 2.5 <= paddleLeft.object.position.x + 5)
+		direction *= -1
+	if (ball.object.position.x >= 203)
 	{
-		//direction *= -1
+		ball.object.position.x = 0
+		ball.pointLight.position.x = 0
 	}
-	//sphere.position.y -= 0.01;
+}
 
+function ballMouvement()
+{
+	ball.object.rotation.y += 1 * direction
+	ball.object.position.x += 1 * direction;
+	ball.pointLight.position.x += 1 *direction;
+}
+
+function game() {
+	requestAnimationFrame( game );
+	ballMouvement()
+	colision()
 	renderer.render( scene, camera );
 }
 
-animate();
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event)
+{
+	var keyCode = event.which;
+	if (keyCode === 38 && paddleRight.object.position.y < 95)
+	{
+		paddleRight.object.position.y += 1
+		paddleRight.rectLight.position.y += 1
+	}
+	else if (event.keyCode === 40 && paddleRight.object.position.y > -95)
+	{
+		paddleRight.object.position.y -= 1
+		paddleRight.rectLight.position.y -= 1
+	}
+}
+
+game();
