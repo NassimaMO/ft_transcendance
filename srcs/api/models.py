fd5ca3f521546298e11f60d73900ca377c884af8
Nassima <nassimamouslim@gmail.com>
2024-06-26 09:29:28 +0200

    basic models created

--- srcs/api/models.py
+++ srcs/api/models.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=125, null=True, blank=True)
     level = models.FloatField(default=0)
-    # friends = models.ManyToManyField("self", blank=True)
+    rank = models.IntegerField(default=0)
     gamesWon = models.FloatField(default=0)
     gamesTotal = models.FloatField(default=0)
     gamesWonMulti = models.FloatField(default=0)
@@ -13,7 +13,7 @@ class Player(models.Model):
 
     def __str__(self):
         return self.user.username
-    
+
 class Friend(models.Model):
     user_from = models.ForeignKey(User, related_name='friend_from', on_delete=models.CASCADE)
     user_to = models.ForeignKey(User, related_name='friend_to', on_delete=models.CASCADE)
@@ -32,13 +32,47 @@ User.add_to_class('friends', models.ManyToManyField(
     related_name='friendship'
 ))
 
-# Example:
+class GameSession(models.Model):
+    player_one = models.ForeignKey(Player, related_name='player_one', on_delete=models.CASCADE)
+    player_two = models.ForeignKey(Player, related_name='player_two', on_delete=models.CASCADE)
+    mode = models.TextField(max_length=125)
+    score = models.IntegerField(default=0)
+    started = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('player_one', 'player_two')
+
+    def __str__(self):
+        return f'{self.playerOne.user.username} is playing with {self.playerTwo.user.username}'
+
+Player.add_to_class('GameHistory', models.ManyToManyField(
+    'self',
+    through=GameSession,
+    symmetrical=False,
+    related_name='gameSessions'
+))
+
+# import logging
 # from django.contrib.auth import authenticate
-# user = User.objects.create_user(username='NassiGigachad', email='NassiGigachad@gmail.com', password='enavantouioui123')
-# user = authenticate(username='NassiGigachad', password='enavantouioui123')
-# if user is not None:
-#     print("Authenticated successfully!")
-# else:
-#     print("Authentication failed.")
-# player = Player.objects.create(user=user, bio='It\'s me')
-#
+
+# logger = logging.getLogger('django')
+
+# def createPlayer(username, password, email)
+#     logger.info("HEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY LISTEEEEEEEEEEEEEEEEEENNNNNNNNNNNN!")
+#     user = User.objects.create_user(username=username, email=password, password=email)
+#     player = Player.objects.create(user=user)
+#     return player
+
+# def authenticatePlayer(username, password):
+#     user = authenticate(username=username, password=password)
+#     if user is not None:
+#         logger.info("Authenticated successfully!")
+#         return True
+#     logger.info("Authentication failed.")
+#     return False
+
+# def friendRequest(user_from_name, user_to_name):
+#     user_from = Player.objects.get(user=user_from_name)
+#     user_to = Player.objects.get(user=user_to_name)
+#     Friend.objects.get_or_create(user_from=user_from, user_to=user_to)
+#     Friend.objects.get_or_create(user_from=user_to, user_to=user_from)
--- srcs/transcendance/settings.py
+++ srcs/transcendance/settings.py
@@ -142,3 +142,26 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'file': {
+#             'level': 'INFO',
+#             'class': 'logging.FileHandler',
+#             'filename': os.path.join(BASE_DIR, 'debug.log'),
+#         },
+#         'console': {
+#             'level': 'INFO',
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file', 'console'],
+#             'level': 'INFO',
+#             'propagate': True,
+#         },
+#     },
+# }
