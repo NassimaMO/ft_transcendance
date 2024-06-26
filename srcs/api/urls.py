b1a8df406e54974a0556b02895795339f5449383
Nassima <nassimamouslim@gmail.com>
2024-06-26 13:01:22 +0200

    problem with player registering

--- srcs/api/urls.py
+++ srcs/api/urls.py
@@ -4,13 +4,22 @@ from .views import (
     GameSessionListCreate,
     GameSessionRetrieveUpdateDestroy,
     FriendListCreate,
-    FriendRetrieveUpdateDestroy
+    FriendRetrieveUpdateDestroy,
+    UserListCreate
 )
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
+
 
 urlpatterns = [
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path("players/", PlayerListCreate.as_view(), name="player-list-create"),
     # path("Player/<int:pk>/", views.PlayerRetrieveUpdateDestroy.as_view(), name="update"),
-    # path("User/", views.UserCreate.as_view(), name="user-views-create"),
+    path("users/", UserListCreate.as_view(), name="user-list-create"),
     # path("User/<int:pk>/", views.UserRetrieveUpdateDestroy.as_view(), name="update"),
     path("friends/", FriendListCreate.as_view(), name="friend-list-create"),
     path("friends/<int:pk>/", FriendRetrieveUpdateDestroy.as_view(), name="friend-update"),
