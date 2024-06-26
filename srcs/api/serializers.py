b58493c6e1d4c744a397b728af9aba4d122589b9
Nassima <nassimamouslim@gmail.com>
2024-06-26 09:29:28 +0200

    basic models created

--- srcs/api/serializers.py
+++ srcs/api/serializers.py
@@ -2,19 +2,28 @@ from rest_framework import serializers
 from .models import Player
 from .models import User
 from .models import Friend
+from .models import GameSession
 
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
-        fields = ["id", "user", "bio", "level", "gamesWon", "gamesTotal", "gamesWonMulti", "gamesWonRegular"]
-
+        fields = ['id', 'user', 'bio', 'level', 'rank', 'gamesWon', 'gamesTotal', 'gamesWonMulti', 'gamesWonRegular']
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password", "email", "is_active", "date_joined"]
+        fields = ['id', 'username', 'email']
 
 class FriendSerializer(serializers.ModelSerializer):
+    user_from = UserSerializer()
+    user_to = UserSerializer()
     class Meta:
         model = Friend
-        fields = ["id", "user_from", "user_to", "created"]
+        fields = ['id', 'user_from', 'user_to', 'created']
+
+class GameSessionSerializer(serializers.ModelSerializer):
+    player_one = PlayerSerializer()
+    player_two = PlayerSerializer()
+    class Meta:
+        model = GameSession
+        fields = ['id', 'player_one', 'player_two', 'mode', 'score', 'started']
--- srcs/api/views.py
+++ srcs/api/views.py
@@ -1,36 +1,38 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Player
-from .models import User
-from .models import Friend
-from .serializers import PlayerSerializer
-from .serializers import UserSerializer
-from .serializers import FriendSerializer
+from .models import Player, User, Friend, GameSession
+from .serializers import PlayerSerializer, UserSerializer, FriendSerializer, GameSessionSerializer
 
-
-class PlayerCreate(generics.ListCreateAPIView):
+class PlayerListCreate(generics.ListCreateAPIView):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
-class PlayerRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Player.objects.all()
-    serializer_class = PlayerSerializer
-    lookup_field = "pk"
+# class PlayerRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Player.objects.all()
+#     serializer_class = PlayerSerializer
+#     lookup_field = "pk"
 
-class UserCreate(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserCreate(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
-class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    lookup_field = "pk"
+# class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     lookup_field = "pk"
 
-class FriendCreate(generics.ListCreateAPIView):
+class FriendListCreate(generics.ListCreateAPIView):
     queryset = Friend.objects.all()
     serializer_class = FriendSerializer
 
 class FriendRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Friend.objects.all()
     serializer_class = FriendSerializer
-    lookup_field = "pk"
+
+class GameSessionListCreate(generics.ListCreateAPIView):
+    queryset = GameSession.objects.all()
+    serializer_class = GameSessionSerializer
+
+class GameSessionRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GameSession.objects.all()
+    serializer_class = GameSessionSerializer
