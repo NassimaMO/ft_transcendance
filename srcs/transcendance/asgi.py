0ebad49a0856cee30714c01ce084cfb0a3f10998
Nily <nghulam@student.42.fr>
2024-07-20 23:51:05 +0200

    update django channels / drf settings

--- srcs/transcendance/asgi.py
+++ srcs/transcendance/asgi.py
@@ -8,15 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from channels.auth import AuthMiddlewareStack
 from transcendance import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendance.settings')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
--- srcs/transcendance/settings.py
+++ srcs/transcendance/settings.py
@@ -83,7 +83,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'transcendance.wsgi.application'
 
+# Channels
+
 ASGI_APPLICATION = 'transcendance.asgi.application'
+
+""" CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+} """
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -99,6 +110,21 @@ DATABASES = {
     }
 }
 
+# API
+
+""" REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+} """
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
