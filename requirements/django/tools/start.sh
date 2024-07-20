385030ea297d8635976ae1a584b7aa8aad3283a2
Nily <nghulam@student.42.fr>
2024-07-20 23:34:51 +0200

    django admin superuser creation
    new python script srcs/create_superuser.py to create a superuser according to .env variables, executed in django container start.sh script
    as this commit require some changes in .env files, a new template is provided with all variables needed and some core values

--- requirements/django/tools/start.sh
+++ requirements/django/tools/start.sh
@@ -3,9 +3,11 @@ cd srcs
 if [ "$DEBUG" -eq 1 ]; then
     python3 manage.py makemigrations
     python3 manage.py migrate
+    python3 create_superuser.py
     python3 manage.py runserver 0.0.0.0:8000
 else
     python3 manage.py collectstatic --no-input
     python3 manage.py migrate
+    python3 create_superuser.py
     daphne -b 0.0.0.0 -p 8000 transcendance.asgi:application
 fi
