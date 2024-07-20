f7244c81da5101bdfac9ca966dc766e5608a0b9a
Nily <nghulam@student.42.fr>
2024-07-20 23:42:05 +0200

    added new front page for pong game, with game mode selection

--- srcs/pong/urls.py
+++ srcs/pong/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+from django.apps import apps
 
 urlpatterns = [
     path('', views.play, name='play'),
+    path('solo/', views.game, name='game'),
+    path('multi/', views.game, name='game'),
+    path('private/', views.private, name='private'),
 ]
