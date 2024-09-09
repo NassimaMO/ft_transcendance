610a2cfebe2f96f049f33edbc34c6473d2973121
Theo BAE <tbae@paul-f4Ar3s2.clusters.42paris.fr>
2024-09-09 11:01:14 +0200

    Fixed issues preventing the game from launching

--- srcs/pong/static/pong/js/animation.js
+++ srcs/pong/static/pong/js/animation.js
@@ -1,6 +1,6 @@
+import { threeJS } from './main.js'
+import * as Geo from './buildGeometry.js'
 let idx = 0
-import { threeJS } from './main'
-import {puck, paddleLeft, paddleRight} from './buildGeometry.js'
 
 export function start()
 {
@@ -8,10 +8,10 @@ export function start()
 	threeJS.camera.rotation.x -= Math.PI / 128
 	threeJS.camera.position.y = (Math.cos((Math.PI / 128) * idx) * 200 * -1)
 	threeJS.camera.position.z = Math.sin((Math.PI / 128) * idx) * 200
-	paddleLeft.object.position.z -= 3
-	paddleLeft.rectLight.position.z -= 3
-	paddleRight.object.position.z -= 3
-	paddleRight.rectLight.position.z -= 3
+	Geo.paddleLeft.object.position.z -= 3
+	Geo.paddleLeft.rectLight.position.z -= 3
+	Geo.paddleRight.object.position.z -= 3
+	Geo.paddleRight.rectLight.position.z -= 3
 	idx++
 	if (idx == 64)
 	{
