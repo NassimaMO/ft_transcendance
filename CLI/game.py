598609ab610f06f5173630f648e9a7ac4e43f9d4
Nassima <nassimamouslim@gmail.com>
2024-10-02 18:28:21 +0200

    Game: take terminal size into account, make the display_paddles function to display only one paddle at a time, make the paddle size dynamic, check ball display bugs, when returning to the menu there might be some input left in the standard input so it displays invalid command even though the command valid

--- CLI/game.py
+++ CLI/game.py
@@ -1,8 +1,14 @@
 import curses, time
 
-ball_symbol = "O"
-paddle_symbol = "H"
-paddle_size = 3
+TABLE_SIZE = [60, 16]
+MAX_SCORE = 3
+BALL_SYMBOL = "O"
+BALL_POS = [30, 7]
+PADDLE_SYMBOL = "H"
+PADDLE_POS = 7
+PADDLE_SIZE = 3
+WIN = 1
+LOSE = 2 
 
 def main(stdscr):
     curses.curs_set(0)   # Hide cursor
@@ -14,101 +20,102 @@ def main(stdscr):
 class Game:
     def __init__(self, stdscr):
         self.stdscr = stdscr
-        self.pong_table_size = [60, 16]
-        self.middle_y, self.middle_x = self.get_terminal_size()
-        self.middle_y = self.middle_y // 2 + self.pong_table_size[1]
-        self.middle_x = self.middle_x // 2 + self.pong_table_size[0]
-        self.enemy_paddle = [7, 'right']
-        self.paddle = [7, 'left']
-        self.prev_ball_pos_x = 30
-        self.prev_ball_pos_y = 7
-        self.ball_pos_x = 30
-        self.ball_pos_y = 7
-        self.ball_velocity_x = 1 # make it random
-        self.ball_velocity_y = 1 # make it random
+        curses.init_pair(WIN, curses.COLOR_WHITE, curses.COLOR_GREEN)
+        curses.init_pair(LOSE, curses.COLOR_WHITE, curses.COLOR_RED)
+        self.middle_y, self.middle_x = self.get_terminal_size() # take terminal size change into account
+        self.middle_y = self.middle_y // 2 - TABLE_SIZE[1] // 2
+        self.middle_x = self.middle_x // 2 - TABLE_SIZE[0] // 2
+        self.enemy_paddle = [PADDLE_POS, 'right']
+        self.paddle = [PADDLE_POS, 'left']
+        self.prev_ball_pos_x = BALL_POS[0]
+        self.prev_ball_pos_y = BALL_POS[1]
+        self.ball_pos_x = BALL_POS[0]
+        self.ball_pos_y = BALL_POS[1]
+        self.ball_velocity_x = 1 # random
+        self.ball_velocity_y = 1 # random
         self.score = [0, 0]
         
     def update(self):
         self.display_pong_table()
         while True:
             self.get_game_logic()
-            self.display_paddles_ball()
-            if self.score[0] == 3:
-                self.stdscr.addstr(self.center(7, 'y'), self.center(17, 'x'), "You have won !")
-                self.stdscr.refresh()
-                time.sleep(1)
+            self.display_ball()
+            self.display_paddles()
+            self.stdscr.refresh()
+            if self.score[0] == MAX_SCORE:
+                self.custom_text("You have won !", WIN)
                 break
-            elif self.score[1] == 3:
-                self.stdscr.addstr(self.center(7, 'y'), self.center(17, 'x'), "You have lost...")
-                self.stdscr.refresh()
-                time.sleep(1)
+            elif self.score[1] == MAX_SCORE:
+                self.custom_text("You have lost...", LOSE)
                 break
             self.controls()
 
     def display_pong_table(self):
         self.stdscr.clear()
-        for x in range(self.pong_table_size[0]):
+        for x in range(TABLE_SIZE[0]):
             self.stdscr.addch(self.center(0, 'y'), self.center(x, 'x'), '-')
-            self.stdscr.addch(self.center(self.pong_table_size[1] - 1, 'y'), self.center(x, 'x'), '-') # centering problem stdscr
+            self.stdscr.addch(self.center(TABLE_SIZE[1] - 1, 'y'), self.center(x, 'x'), '-')
     
-        for y in range(self.pong_table_size[1]):
+        for y in range(TABLE_SIZE[1]):
             self.stdscr.addch(self.center(y, 'y'), self.center(0, 'x'), '|')
-            self.stdscr.addch(self.center(y, 'y'), self.center((self.pong_table_size[0] - 1), 'x'), '|')
+            self.stdscr.addch(self.center(y, 'y'), self.center((TABLE_SIZE[0] - 1), 'x'), '|')
 
-        self.stdscr.refresh()
+    def display_paddles(self, paddle_size=PADDLE_SIZE): # might separate ennemy_paddle and paddle
+        for i in range(-1, 2): # make it using the paddle size
+            self.stdscr.addch(self.center(self.paddle[0] + i, 'y'), self.center(2, 'x'), PADDLE_SYMBOL)
+            self.stdscr.addch(self.center(self.enemy_paddle[0] + i, 'y'), self.center(TABLE_SIZE[0] - 3, 'x'), PADDLE_SYMBOL)
 
-    def display_paddles_ball(self):
-        for i in range(-1, 2):
-            self.stdscr.addch(self.center(self.paddle[0] + i, 'y'), self.center(2, 'x'), paddle_symbol)
-            self.stdscr.addch(self.center(self.enemy_paddle[0] + i, 'y'), self.center(self.pong_table_size[0] - 3, 'x'), paddle_symbol)
-
+    def display_ball(self):
         self.stdscr.addch(self.center(self.prev_ball_pos_y, 'y'), self.center(self.prev_ball_pos_x, 'x'), ' ')
-        self.stdscr.addch(self.center(self.ball_pos_y, 'y'), self.center(self.ball_pos_x, 'x'), ball_symbol)
+        self.stdscr.addch(self.center(self.ball_pos_y, 'y'), self.center(self.ball_pos_x, 'x'), BALL_SYMBOL)
         self.prev_ball_pos_x = self.ball_pos_x
         self.prev_ball_pos_y = self.ball_pos_y
-
-        self.stdscr.refresh()
 
     def controls(self):
         #action = await input("> Enter a move (UP/DOWN/MENU): ")
         key = self.stdscr.getch()
-        if key == ord('w') and self.paddle[0] > 1:
+        if key == ord('w') and self.paddle[0] > 2:
             self.stdscr.addch(self.center(self.paddle[0] + 1, 'y'), self.center(2, 'x'), ' ')
             self.paddle[0] -= 1
-        elif key == ord('s') and self.paddle[0] < 14:
+        elif key == ord('s') and self.paddle[0] < TABLE_SIZE[1] - 3:
             self.stdscr.addch(self.center(self.paddle[0] - 1, 'y'),  self.center(2, 'x'), ' ')
             self.paddle[0] += 1
-        if key == curses.KEY_UP and self.enemy_paddle[0] > 1:
-            self.stdscr.addch(self.center(self.enemy_paddle[0] + 1, 'y'),  self.center(self.pong_table_size[0] - 3, 'x'), ' ')
+        if key == curses.KEY_UP and self.enemy_paddle[0] > 2:
+            self.stdscr.addch(self.center(self.enemy_paddle[0] + 1, 'y'),  self.center(TABLE_SIZE[0] - 3, 'x'), ' ')
             self.enemy_paddle[0] -= 1
-        elif key == curses.KEY_DOWN and self.enemy_paddle[0] < 14:
-            self.stdscr.addch(self.center(self.enemy_paddle[0] - 1, 'y'),  self.center(self.pong_table_size[0] - 3, 'x'), ' ')
+        elif key == curses.KEY_DOWN and self.enemy_paddle[0] < TABLE_SIZE[1] - 3:
+            self.stdscr.addch(self.center(self.enemy_paddle[0] - 1, 'y'),  self.center(TABLE_SIZE[0] - 3, 'x'), ' ')
             self.enemy_paddle[0] += 1
 
     def get_game_logic(self):
         self.ball_pos_x += self.ball_velocity_x
         self.ball_pos_y += self.ball_velocity_y
 
-        if self.ball_pos_y >= 14 or self.ball_pos_y <= 1:
+        if self.ball_pos_y >= TABLE_SIZE[1] - 2 or self.ball_pos_y <= 1:
             self.ball_velocity_y *= -1
 
-        if self.ball_pos_x > 57:
+        if self.ball_pos_x > TABLE_SIZE[0] - 2:
             self.score[0] += 1
-            self.stdscr.addstr(self.center(7, 'y'), self.center(24, 'x'), "Point for You !")
-            self.stdscr.refresh()
+            self.custom_text("Point for player", WIN)
             self.reset()
-        elif self.ball_pos_x < 2:
+        elif self.ball_pos_x < 1:
             self.score[1] += 1
-            self.stdscr.addstr(self.center(7, 'y'), self.center(17, 'x'), "Point for the enemy...")
-            self.stdscr.refresh()
+            self.custom_text("Point for enemy_player", LOSE)
             self.reset()
 
         if self.is_ball_colliding_with_paddle(self.paddle) or self.is_ball_colliding_with_paddle(self.enemy_paddle):
             self.ball_velocity_x *= -1
 
+    def custom_text(self, text, color, t=1):
+        self.stdscr.attron(curses.color_pair(color) | curses.A_BOLD)
+        self.stdscr.addstr(self.center(-3, 'y'), self.center(TABLE_SIZE[0] // 2 - len(text) // 2 , 'x'), text)
+        self.stdscr.attroff(curses.color_pair(color) | curses.A_BOLD)
+        self.stdscr.refresh()
+        time.sleep(t)
+        self.stdscr.addstr(self.center(-3, 'y'), self.center(TABLE_SIZE[0] // 2 - len(text) // 2, 'x'), ' ' * len(text))
 
     def is_ball_colliding_with_paddle(self, paddle):
-        paddle_x = 57 if paddle[1] == 'right' else 2
+        paddle_x = TABLE_SIZE[0] - 2 if paddle[1] == 'right' else 1
         paddle_y_min = paddle[0] - 1
         paddle_y_max = paddle[0] + 1
 
@@ -118,16 +125,13 @@ class Game:
         return within_x_bounds and within_y_bounds
 
     def reset(self):
-        time.sleep(1)
-        self.stdscr.addstr(self.center(7, 'y'), self.center(17, 'x'), "                              ")
         for i in range(-1, 2):
             self.stdscr.addch(self.center(self.paddle[0] + i, 'y'),  self.center(2, 'x'), ' ')
-            self.stdscr.addch(self.center(self.enemy_paddle[0] + i, 'y'),  self.center(self.pong_table_size[0] - 3, 'x'), ' ')
-        self.paddle[0] = 7
-        self.enemy_paddle[0] = 7
-        self.stdscr.refresh()
-        self.ball_pos_x = 30
-        self.ball_pos_y = 7
+            self.stdscr.addch(self.center(self.enemy_paddle[0] + i, 'y'),  self.center(TABLE_SIZE[0] - 3, 'x'), ' ')
+        self.paddle[0] = PADDLE_POS
+        self.enemy_paddle[0] = PADDLE_POS
+        self.ball_pos_x = BALL_POS[0]
+        self.ball_pos_y = BALL_POS[1]
         self.ball_velocity_x *= -1
 
     def center(self, coord, xy):
