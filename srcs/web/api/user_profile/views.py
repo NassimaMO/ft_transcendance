fddd3dbca30cd5cd4fd60dffdd3f15cd0dbd413e
Nassima <nassimamouslim@gmail.com>
2025-01-16 18:18:37 +0100

    frontend: dashboard display changed, dynamic history in progress

--- srcs/web/api/user_profile/views.py
+++ srcs/web/api/user_profile/views.py
@@ -24,19 +24,42 @@ class UserStatisticsView(APIView):
     def get(self, request):
         user = request.user
         all_games_played = user.get_all_games_played()
+        history = self._get_history(all_games_played, user)
         games_played = len(all_games_played)
         games_won = self._get_total_games_won(all_games_played, user)
         win_streak = self._get_win_streak(all_games_played, user)
         average_score = self._get_average_score(all_games_played, user)
         win_loss_ratio = games_won / (games_played - games_won) if games_played != games_won else games_won
         data = {
+            "history" : history,
             "total_games_played": games_played,
             "games_won": games_won,
+            "games_lost": games_played - games_won,
             "win_streak": win_streak,
             "average_score": average_score,
             "win_loss_ratio": win_loss_ratio,
         }
         return Response(data)
+    
+    @classmethod
+    def _get_history(self, all_games_played, user):
+        history = []
+        for match in all_games_played:
+            matches = {}
+            winning_team = Team.objects.filter(match=match).order_by('-score').first()
+            if winning_team and winning_team.players.filter(id=user.id).exists():
+                matches["result"] = "Victory"
+                matches["team1_score"] = winning_team.score
+                matches["team2_score"] = Team.objects.filter(match=match).exclude(id=winning_team.id).first()
+
+            else:
+                matches["result"] = "Defeat"
+                matches["team1_score"] = Team.objects.filter(match=match).exclude(id=winning_team.id).first()
+                matches["team2_score"] = winning_team.score
+            matches["mode"] = match.info.mode
+            matches["date"] = match.date
+            history.append(matches)
+        return history
 
     @classmethod
     def _get_total_games_won(self, all_games_played, user):
