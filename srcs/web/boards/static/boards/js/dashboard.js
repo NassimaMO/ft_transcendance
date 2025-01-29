af38b95ebcc84a03e2db251813655e564fa1e81f
Nassima <nassimamouslim@gmail.com>
2025-01-29 11:59:10 +0100

    frontend: charts done, need to connect them and smoothen the transition

--- srcs/web/boards/static/boards/js/dashboard.js
+++ srcs/web/boards/static/boards/js/dashboard.js
@@ -1,10 +1,11 @@
+
 document.querySelectorAll('.display-button').forEach(function(button) {
   button.addEventListener('click', function() {
     var contentToHide = document.querySelector('.statistics');
     var otherContentToHide = document.querySelector('.streak');
     var infoContentToHide = document.querySelector('.more-info');
     var newContent = document.querySelector('.overlay-info');
-
+    
     if (contentToHide.classList.contains('hidden')) {
       contentToHide.classList.remove('hidden');
       otherContentToHide.classList.remove('hidden');
@@ -19,4 +20,41 @@ document.querySelectorAll('.display-button').forEach(function(button) {
       otherContentToHide.style.display = "none";
     }
   });
+});
+
+var ctx = document.getElementById('avg-score-graph').getContext('2d');
+
+var matchScoresChart = new Chart(ctx, {
+type: 'line',
+data: {
+    labels: ['12/04/2021', '05/08/2019', '23/11/2020', '15/06/2018', '09/02/2023', '30/09/2022', '17/12/2020', '25/07/2017', '02/03/2021', '19/10/2019'],
+    datasets: [{
+        data: [1, 2, 2, 4, 5, 5, 3, 4, 0, 4],
+    }]
+},
+options: {
+    responsive: true,
+    scales: {
+        x: {
+            display: false,
+        },
+        y: {
+            title: {
+                display: false,
+            },
+            ticks: {
+                display: true,
+            },
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false,
+        },
+        tooltip: {
+            enabled: true,
+        }
+    }
+}
 });
