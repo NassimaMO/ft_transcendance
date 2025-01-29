dbec84681f06b551e34d99877c56cd8cacbf0fa2
Nassima <nassimamouslim@gmail.com>
2025-01-29 09:25:40 +0100

    dashboard display transition done

--- srcs/web/boards/static/boards/js/dashboard.js
+++ srcs/web/boards/static/boards/js/dashboard.js
@@ -1,14 +1,22 @@
 document.querySelectorAll('.display-button').forEach(function(button) {
   button.addEventListener('click', function() {
-
     var contentToHide = document.querySelector('.statistics');
     var otherContentToHide = document.querySelector('.streak');
     var infoContentToHide = document.querySelector('.more-info');
     var newContent = document.querySelector('.overlay-info');
 
-    contentToHide.style.display = contentToHide.style.display === "none" ? "flex" : "none";
-    otherContentToHide.style.display = otherContentToHide.style.display === "none" ? "block" : "none";
-    newContent.style.display = newContent.style.display === "block" ? "none" : "block";
-    infoContentToHide.style.display = infoContentToHide.style.display === "none" ? "block" : "none";
+    if (contentToHide.classList.contains('hidden')) {
+      contentToHide.classList.remove('hidden');
+      otherContentToHide.classList.remove('hidden');
+      newContent.style.display = "none";
+      infoContentToHide.style.display = "block";
+      otherContentToHide.style.display = "block";
+    } else {
+      contentToHide.classList.add('hidden');
+      otherContentToHide.classList.add('hidden');
+      newContent.style.display = "block";
+      infoContentToHide.style.display = "none";
+      otherContentToHide.style.display = "none";
+    }
   });
 });
