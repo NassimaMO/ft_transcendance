79d0d6951950db6e3caa42ee9d5e0cfb22e70b94
Nassima <nassimamouslim@gmail.com>
2025-03-29 17:21:33 +0100

    average score chart connected to backend

--- srcs/web/boards/views.py
+++ srcs/web/boards/views.py
@@ -12,11 +12,15 @@ def boards_view(request):
     stats = UserStatsSerializer(request.user)
     rank = UserMeRankView.get(request, 'pong')
     matches = request.user.get_matches()
+    match_dates = [match.date.strftime('%d/%m/%Y') for match in matches]
+    match_scores = [match.teams.all().first().score if match.teams.exists() else 0 for match in matches]
     history_data = UserHistorySerializer(matches, many=True)
     context = {
         "stats": stats.data,
         "rank": rank.data,
-        "history": history_data.data
+        "history": history_data.data,
+        "match_dates": match_dates,
+        "match_scores": match_scores
     }
     logger.info(f"{context}")
     return render(request, "boards/boards.html", context)
